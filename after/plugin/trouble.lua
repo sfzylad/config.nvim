641aa367a28bf415dbb9024beb623e312883b966
Dominik Zyla <dzyla@vmware.com>
2023-10-25 14:45:59 +0100

    Add Trouble plugin key bindings.

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,3 +1,10 @@
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
     {silent=true, noremap=true}
 )
+
+vim.keymap.set("n", "<leader>tr", "<cmd>TroubleToggle lsp_references<cr>")
+vim.keymap.set("n", "<leader>td", "<cmd>TroubleToggle lsp_type_definitions<cr>")
+vim.keymap.set("n", "<leader>ti", "<cmd>TroubleToggle lsp_implementations<cr>")
+vim.keymap.set("n", "<leader>ti", "<cmd>TroubleToggle lsp_implementations<cr>")
+vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle document_diagnostics<cr>")
+
