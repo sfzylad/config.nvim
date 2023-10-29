3471de6b4c22868ade155a70780cd92a51a4d713
Dominik Zyla <dzyla@vmware.com>
2023-10-29 21:04:28 +0000

    Don't overwrite highlights.

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -6,8 +6,8 @@ function ColorMyPencils(color)
     color = color or "rose-pine"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     require("zylad.config.ui").setup()
 end
 
