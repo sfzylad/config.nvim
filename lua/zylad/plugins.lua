5b0f5f185a479ee0f8d6a6c8c0e643cbc23b5818
Dominik Zyla <dzyla@vmware.com>
2024-07-05 13:28:52 +0100

    Not using mason, so don't load it.

--- lua/zylad/plugins.lua
+++ lua/zylad/plugins.lua
@@ -19,18 +19,18 @@ require("zylad.config.bufferline").setup()
 -----------------------------------------------------------------------------
 -- mason
 -----------------------------------------------------------------------------
-require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "gopls" },
-}
+-- require("mason").setup({
+--     ui = {
+--         icons = {
+--             package_installed = "✓",
+--             package_pending = "➜",
+--             package_uninstalled = "✗"
+--         }
+--     }
+-- })
+-- require("mason-lspconfig").setup {
+--     ensure_installed = { "lua_ls", "gopls" },
+-- }
 
 -----------------------------------------------------------------------------
 -- git worktree
