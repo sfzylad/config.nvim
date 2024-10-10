2b5db5535d9c16e372fe585f10729290585ab46c
Dominik Zyla <dominik.zyla@gmail.com>
2024-10-10 21:54:37 +0100

    Clean up the plugins file.

--- lua/zylad/plugins.lua
+++ lua/zylad/plugins.lua
@@ -17,22 +17,6 @@ require("zylad.config.bufferline").setup()
 
 
 -----------------------------------------------------------------------------
--- mason
------------------------------------------------------------------------------
--- require("mason").setup({
---     ui = {
---         icons = {
---             package_installed = "✓",
---             package_pending = "➜",
---             package_uninstalled = "✗"
---         }
---     }
--- })
--- require("mason-lspconfig").setup {
---     ensure_installed = { "lua_ls", "gopls" },
--- }
-
------------------------------------------------------------------------------
 -- git worktree
 -----------------------------------------------------------------------------
 require("git-worktree").setup({
@@ -47,47 +31,6 @@ require("git-worktree").setup({
 -- gitsigns.nvim setup
 ----------------------------------------------------------------------------
 require('gitsigns').setup()
-
-----------------------------------------------------------------------------
--- indent indent_blankline setup
-----------------------------------------------------------------------------
--- local highlight = {
---     "RainbowRed",
---     "RainbowYellow",
---     "RainbowBlue",
---     "RainbowOrange",
---     "RainbowGreen",
---     "RainbowViolet",
---     "RainbowCyan",
--- }
---
--- local hooks = require "ibl.hooks"
--- -- create the highlight groups in the highlight setup hook, so they are reset
--- -- every time the colorscheme changes
--- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
---     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
---     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
---     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
---     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
---     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
---     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
---     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
--- end)
---
--- require("ibl").setup { indent = { highlight = highlight } }
-
--- local highlight = {
---     "CursorColumn",
---     "Whitespace",
--- }
--- require("ibl").setup {
---     indent = { highlight = highlight, char = "" },
---     whitespace = {
---         highlight = highlight,
---         remove_blankline_trail = false,
---     },
---     scope = { enabled = false },
--- }
 
 require "ibl".setup({
     scope = { enabled = true },
@@ -159,7 +102,7 @@ require('tabline').setup({
 -- require('leap').add_default_mappings()
 
 -- NOTE: This has to go at the end
-require("zylad.config.ui").setup()
+-- require("zylad.config.ui").setup()
 
 -- This module contains a number of default definitions
 -- local rainbow_delimiters = require 'rainbow-delimiters'
