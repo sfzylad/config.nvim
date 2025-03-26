8bf21e17e9d0bb555c39935dd4b12da8928e71d8
Dominik Zyla <22446399+sfzylad@users.noreply.github.com>
2025-03-26 18:59:08 +0000

    Fix the formatting in golang with null_ls magic.

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -111,6 +111,8 @@ return {
           local methods = require("null-ls.methods")
           local helpers = require("null-ls.helpers")
 
+          local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
           local function ruff_fix()
               return helpers.make_builtin({
                   name = "ruff",
@@ -128,16 +130,52 @@ return {
                   factory = helpers.formatter_factory
               })
           end
+          local function gofumpt()
+            local h = require("null-ls.helpers")
+            local methods = require("null-ls.methods")
+            local FORMATTING = methods.internal.FORMATTING
+            return helpers.make_builtin({
+                name = "gofumpt",
+                meta = {
+                    url = "https://github.com/mvdan/gofumpt",
+                    description = "Enforce a stricter format than gofmt, while being backwards compatible. That is, gofumpt is happy with a subset of the formats that gofmt is happy with.",
+                },
+                method = FORMATTING,
+                filetypes = { "go" },
+                generator_opts = {
+                    command = "gofumpt",
+                    to_stdin = true,
+                },
+                factory = h.formatter_factory,
+            })
+          end
 
           null_ls.setup({
+              on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+
               sources = {
-                  -- ruff_fix(),
+                  ruff_fix,
                   -- null_ls.builtins.diagnostics.ruff,
                   -- null_ls.builtins.formaters.black,
                   -- require("none-ls.diagnostics.ruff"),
                   -- require("none-ls.diagnostics.mypy"),
                   -- require("none-ls.formatting.black"),
+                  gofumpt,
                   null_ls.builtins.diagnostics.ruff,
+                  null_ls.builtins.formatting.gofumpt,
                   -- require("null_ls.builtins.diagnostics.ruff"),
                   -- require("null_ls.builtins.diagnostics.mypy"),
                   -- require("null_ls.builtins.formatting.black"),
