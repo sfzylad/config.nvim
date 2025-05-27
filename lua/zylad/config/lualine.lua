97ec0e8ce6ae030dc9402e6d5e1885f82742eaa9
Dominik Zyla <22446399+sfzylad@users.noreply.github.com>
2025-05-27 18:32:13 +0100

    Update the lualine configuration to use its own tabline implementation.

--- lua/zylad/config/lualine.lua
+++ lua/zylad/config/lualine.lua
@@ -62,10 +62,11 @@ local config = {
       },
       ignore_focus = {},
       always_divide_middle = true,
+      always_show_tabline = true,
       globalstatus = false,
       refresh = {
         statusline = 1000,
-        -- tabline = 1000,
+        tabline = 1000,
         winbar = 1000,
       }
     },
@@ -80,12 +81,22 @@ local config = {
     },
 
     inactive_sections = {
-      lualine_a = {'mode'},
+      -- lualine_a = {'mode'},
+      lualine_a = {},
       lualine_b = {},
       lualine_c = {'filename'},
       lualine_x = {'location'},
       lualine_y = {},
-      lualine_z = {'filetype'},
+      -- lualine_z = {'filetype'},
+      lualine_z = {},
+    },
+    tabline = {
+      lualine_a = {'buffers'},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {'tabs'},
     },
     extensions = { "nvim-tree", "toggleterm", "quickfix" },
 }
