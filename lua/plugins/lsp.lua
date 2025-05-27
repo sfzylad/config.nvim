7c9c78d8700ef792b40fc98a71ff9e39036a6971
Dominik Zyla <22446399+sfzylad@users.noreply.github.com>
2025-05-27 19:41:19 +0100

    Change configuration of the indent-blankline.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,6 +38,23 @@ return {
     },
     {
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        config = function ()
+            local highlight = {
+                "CursorColumn",
+                "Whitespace",
+            }
+            require("ibl").setup{
+                indent = { highlight = highlight, char = "" },
+                whitespace = {
+                    highlight = highlight,
+                    remove_blankline_trail = false,
+                },
+                scope = { enabled = true },
+
+            }
+        end
     },
     {
         "mfussenegger/nvim-dap",
